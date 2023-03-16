54a0ee6c3982cb6bb7b487c427da3a0d0f1e327c
Juan Duarte <juanduarte1223@outlook.es>
2023-03-16 13:55:19 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'PipesApp';
+  nombre: string = 'Juan';
+  valor: number = 1000;
+  obj = {
+    name: 'Juan',
+    age: 21,
+    experience: [
+      {
+        job: 'Software Engineer',
+        time: '1 year',
+      },
+      {
+        job: 'Fullstack Developer',
+        time: '2 years',
+      },
+    ],
+  };
+
+  peliculas = [
+    {
+      titulo: 'Avengers',
+      actores: ['Robert Downey Jr', 'Mark Ruffalo', 'Chris Evans'],
+    },
+    {
+      titulo: 'Titanic',
+      actores: ['Leonardo Dicaprio'],
+    },
+  ];
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
